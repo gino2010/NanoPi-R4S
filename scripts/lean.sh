9deb1692e51cc0fef84e4aaf76b51fe96169eb97
Gino <2063598+gino2010@users.noreply.github.com>
2021-04-17 10:35:22 +0800

    update script, disable can module

--- scripts/lean.sh
+++ scripts/lean.sh
@@ -149,6 +149,7 @@ sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i "s/OpenWrt /OpenWrt build @ $(TZ=UTC-8 date "+%Y.%m.%d") /g" package/lean/default-settings/files/zzz-default-settings
 
 # Custom configs
 git am $GITHUB_WORKSPACE/patches/lean/*.patch
